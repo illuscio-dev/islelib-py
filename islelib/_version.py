db2e7ea42023f84d996e963412ae19d86be90414
Billy Peake <b.peake@illuscio.com>
2018-10-18 13:59:22 -0700

    simplified versioning for readthedocs

--- islelib/_version.py
+++ islelib/_version.py
@@ -1,8 +1,1 @@
-import pathlib
-from configparser import ConfigParser
-
-CONFIG_PATH = pathlib.Path(__file__).parent.parent / "setup.cfg"
-config = ConfigParser()
-config.read(CONFIG_PATH)
-
-__version__: str = config.get("bumpversion", "current_version")
+__version__: str = "0.1.0"
