61d12c8934814c1bcc3d8aad5d390cef712649c6
Billy Peake <b.peake@illuscio.com>
2018-10-18 15:09:26 -0700

    error raise for readthedocs debugging

--- islelib/_version.py
+++ islelib/_version.py
@@ -1,8 +1,13 @@
 import pathlib
 from configparser import ConfigParser
 
-CONFIG_PATH = pathlib.Path(__file__).parent.parent / "setup.cfg"
+CONFIG_PATH: pathlib.Path = pathlib.Path(__file__).parent.parent / "setup.cfg"
+CONFIG_PATH: str = str(CONFIG_PATH.absolute())
+
+raise ValueError(CONFIG_PATH)
+
 config = ConfigParser()
 config.read(CONFIG_PATH)
 
 __version__: str = config.get("bumpversion", "current_version")
+print(__version__)
